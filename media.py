44374f5fb5960ac5dbd7d1b6c32c0f11e719008b
Kayane Assi <kayane.assi2017@gmail.com>
2023-04-24 12:09:10 -0300

    commit inicial

---
+++ media.py
@@ -0,0 +1,23 @@
+#-*-coding: utf-8-*-
+
+import os
+import sys
+import time
+
+os.system('clear')
+
+nota1=0
+nota2=0
+nota3=0
+
+nota1 = float(input('digite nota1:'))
+nota2 = float(input('digite nota2:'))
+
+media = (nota1+nota2)/2
+
+print('a media é:', media)
+
+print('\n')
+time.sleep(5)
+
+sys.exit
